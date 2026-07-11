41d361765fea8d801e998f5b34e73e8d739cb9da
Manavkumar2332 <138212313+Manavkumar2332@users.noreply.github.com>
2026-07-11 20:33:35 +0000

    prism

---
+++ Patterns/seventh_prism.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   
+
+   for(int i=1; i<=5-i;i++)
+   {
+    cout<<" ";
+   }
+
+   for(int i=0; i<=2*i+1;i++)
+   {
+    cout<<"*";
+   }
+
+    for(int i=1; i<=5-i;i++)
+   {
+    cout<<" ";
+   }
+   cout<<endl;
+}
