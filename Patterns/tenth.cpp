f5351f066952999bca09e29a9e41d93525746821
manav <manavkum23@gmail.com>
2026-07-13 03:17:32 +0530

    pattern

---
+++ Patterns/tenth.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }cout<<endl;
+
+       
+    }
+
+for(int i=1;i<=5;i++){
+     for(int j=4;j>=i;j--)
+        {
+            cout<<"*";
+        }cout<<endl;
+    }
+}
+
+/* output- 
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+*/
