fcdba78e63bb0b619f02f00583eb265f850cc7f0
manav <manavkum23@gmail.com>
2026-07-18 01:35:17 +0530

    pairs

---
+++ Stl_cpp/tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int,int> p = {1,2};
+
+    cout<<p.first<<p.second;
+
+    pair<int,pair<int,int>>d={1,{2,3}};
+
+    cout<<d.first<<d.second.second<<endl;
+
+    pair<int,int> arr[]={{1,2},{2,3}};
+
+    cout<<arr[1].second;
+
+    vector <pair<int,int>> v;
+
+    v.push_back({1,2});
+    v.push_back({3,4});
+
+    cout<<v[0].first;
+
+
+}
