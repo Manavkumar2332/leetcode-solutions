5a5f112dbbf9ec323350ac30fbe42988b2f36ebf
manav <manavkum23@gmail.com>
2026-07-24 18:30:13 +0530

    reverse an array

---
+++ recurssion/revrse_array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fun(int arr[],int i)
+{
+    if(i== -1)
+    {
+        return 0;
+    }
+
+     cout<<arr[i];
+    fun(arr,i-1);
+
+ 
+   
+
+ 
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    fun(arr,4);
+}
